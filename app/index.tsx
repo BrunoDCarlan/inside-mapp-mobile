0faeb620532469829cb467da80b0e3a6451f7e68
BrunoDCarlan <brunodc04@gmail.com>
2025-05-30 00:12:26 -0300

    :construction: Esboço da tela inicial

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,22 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { Link } from 'expo-router';
 import styles from '../styles/index';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Bem-vindo ao Inside Mapp!</Text>
+      <Image
+        source={require('../assets/logo-com-letreiro.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>Bem-vindo ao Inside Mapp</Text>
+      <Text style={styles.subtitle}>O mapa que pensa em todos os passos</Text>
+
       <Link href="/login" asChild>
-        <Button title="Entrar" />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </TouchableOpacity>
       </Link>
     </View>
   );
